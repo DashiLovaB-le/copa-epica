88c6ba83cc22c2cdf36cd6fabe91a391b5d4526b
DashiTecnology <dashicorporate@gmail.com>
2026-06-12 12:55:49 -0300

    fix: corrige cast no join oid da migration

--- supabase/migrations/20260612160000_remove_carcontrol_trigger.sql
+++ supabase/migrations/20260612160000_remove_carcontrol_trigger.sql
@@ -4,22 +4,28 @@
 DO $$
 DECLARE
   v_tgname TEXT;
+  v_func_oid OID;
   v_proname TEXT;
   v_nspname TEXT;
 BEGIN
   -- Encontra triggers em auth.users cuja funcao contem referencia a carcontrol_user
-  FOR v_tgname, v_proname, v_nspname IN
-    SELECT t.tgname, p.proname, n.nspname
-    FROM pg_trigger t
-    JOIN pg_proc p ON t.tgfoid = p.oid
-    JOIN pg_namespace n ON p.pronamespace = n.nspname
-    WHERE t.tgrelid = 'auth.users'::regclass
-      AND NOT t.tgisinternal
-      AND p.prosrc ILIKE '%carcontrol_user%'
+  FOR v_tgname, v_func_oid IN
+    SELECT tgname, tgfoid
+    FROM pg_trigger
+    WHERE tgrelid = 'auth.users'::regclass
+      AND NOT tgisinternal
   LOOP
-    EXECUTE format('DROP TRIGGER IF EXISTS %I ON auth.users', v_tgname);
-    EXECUTE format('DROP FUNCTION IF EXISTS %I.%I CASCADE', v_nspname, v_proname);
-    RAISE NOTICE 'Removido trigger: %, funcao: %.%', v_tgname, v_nspname, v_proname;
+    SELECT p.proname, n.nspname INTO v_proname, v_nspname
+    FROM pg_proc p
+    JOIN pg_namespace n ON p.pronamespace = n.oid
+    WHERE p.oid = v_func_oid
+      AND p.prosrc ILIKE '%carcontrol_user%';
+
+    IF FOUND THEN
+      EXECUTE format('DROP TRIGGER IF EXISTS %I ON auth.users', v_tgname);
+      EXECUTE format('DROP FUNCTION IF EXISTS %I.%I CASCADE', v_nspname, v_proname);
+      RAISE NOTICE 'Removido trigger: %, funcao: %.%', v_tgname, v_nspname, v_proname;
+    END IF;
   END LOOP;
 END;
 $$;
